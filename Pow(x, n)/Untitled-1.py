01beb7cfb4f062ae8d28888d9fe9446cc2db1092
sudeff <100724247+sudeff@users.noreply.github.com>
2025-03-28 17:23:49 +0300

    code updated

--- Pow(x, n)/Untitled-1.py
+++ Pow(x, n)/Untitled-1.py
@@ -2,8 +2,7 @@ class Solution:
     def Pow(self,x,n):
         return x**n
 
-if __name__=="__main__":
-    solution= Solution()
-    print(solution.Pow(5,6))
+solution= Solution()
+print(solution.Pow(5,6))
 
-#init function created 
+
